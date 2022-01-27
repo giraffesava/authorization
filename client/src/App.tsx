57f3fd9a1b4332f0701890fedd6570162eb053c3
Sava <savely54@mail.ru>
2022-01-28 00:16:36 +0700

    init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,64 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { FC, useContext, useEffect, useState } from "react";
+import LoginForm from "./componets/LoginForm";
+import { Context } from "./index";
+import { observer } from "mobx-react-lite";
+import { IUser } from "./models/IUser";
+import UserService from "./services/UserServise";
 
-function App() {
+const App: FC = () => {
+  const { store } = useContext(Context);
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      store.checkAuth();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  if (store.isLoading) {
+    return <div>Загрузка...</div>;
+  }
+
+  if (!store.isAuth) {
+    return (
+      <div>
+        <LoginForm />
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        {store.isAuth
+          ? `Пользователь авторизован ${store.user.email}`
+          : "АВТОРИЗУЙТЕСЬ"}
+      </h1>
+      <h1>
+        {store.user.isActivated
+          ? "Аккаунт подтвержден по почте"
+          : "ПОДТВЕРДИТЕ АККАУНТ!!!!"}
+      </h1>
+      <button onClick={() => store.logout()}>Выйти</button>
+      <div>
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+      {users.map((user) => (
+        <div key={user.email}>{user.email}</div>
+      ))}
     </div>
   );
-}
+};
 
-export default App;
+export default observer(App);
