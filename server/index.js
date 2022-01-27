f3c503cf5f1e1fd1c5361fdf5849fe4406f23daf
Sava <savely54@mail.ru>
2022-01-27 22:03:27 +0700

    init

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config({});
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const router = require("./router");
+const errorMiddleware = require("./middlewares/error-middleware");
+
+const port = process.env.PORT || 8000;
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use("/api", router);
+app.use(errorMiddleware);
+
+const start = (async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(port, () => console.log(`Server started on PORT ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+})();
